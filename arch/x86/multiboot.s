fc0a1f610f7bbcc5a26a2b414c4cbb2e9c100fea
Mikael Voss <mvs@nyantec.com>
2015-06-06 18:57:35 +0200

    Specify address and entry address tags
    This allows loading by bootloaders that do not support ELF64.

--- arch/x86/multiboot.s
+++ arch/x86/multiboot.s
@@ -9,6 +9,27 @@ mb_start:
 	.long mb_end - mb_start /* Header length */
 	.long -(0xe85250d6 + (mb_end - mb_start)) /* Checksum */
 
+	/* Address tag */
+	.align 8
+mb_tag_addr_start:
+	.short 2
+	.short 1 /* Optional */
+	.long mb_tag_addr_end - mb_tag_addr_start
+	.long mb_start /* Multiboot header address */
+	.long .text     /* Load address */
+	.long _erodata  /* Load end address */
+	.long _end      /* BSS end address */
+mb_tag_addr_end:
+
+	/* Entry address tag */
+	.align 8
+mb_tag_entry_start:
+	.short 3
+	.short 1 /* Optional */
+	.long mb_tag_entry_end - mb_tag_entry_start
+	.long _start      /* Entry address */
+mb_tag_entry_end:
+
 	/* Console flags tag */
 	.align 8
 mb_tag_cons_start:
