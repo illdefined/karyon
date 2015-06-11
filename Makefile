2e3aa5899698ed8e40448197605f82d269444f3b
Mikael Voss <mvs@nyantec.com>
2015-06-11 17:08:28 +0200

    Completely rework makefile

--- Makefile
+++ Makefile
@@ -1,18 +1,5 @@
-RUSTC := rustc
+-include version.mk
+include x86.mk
 
-target/x86/ge: $(addprefix target/x86/, multiboot.o init.o ge.o)
-	$(LD) -T arch/x86/ge.ld -z max-page-size=4096 -static -o $@ $^
-
-target/x86/grub.iso: target/x86/ge
-	grub-mkrescue -o $@ $^ arch/x86/iso
-
-qemu-x86: target/x86/grub.iso
-	qemu-system-x86_64 -cpu kvm64 -smp cores=2,threads=2,sockets=1 -m 256M -cdrom $< -name karyon -display sdl -monitor vc
-
-target/x86/%.o: arch/x86/%.s
-	nasm -f elf64 -o $@ $<
-
-target/x86/%.o: arch/x86/%.rs
-	$(RUSTC) --emit obj -O -o $@ $<
-
-.PHONY: qemu-x86
+version.mk: version.sed $(addprefix .git/, $(shell test -d .git && cut -d ' ' -f 2 .git/HEAD))
+	git describe --dirty --long --always | sed -f version.sed >$@
---
+++ x86.mk
@@ -0,0 +1,41 @@
+x86-LD    := ld
+x86-AS    := nasm
+x86-RUSTC := rustc
+
+x86-cpu   := x86-64
+x86-attr  := +64bit,+avx,+call-reg-indirect,+cmov,+cx16,+lzcnt,+popcnt,+sse,+sse2,+sse4.1,+sse4.2,+ssse3
+
+x86-LDFLAGS   := -b elf64-x86-64 -z max-page-size=0x1000 -static --gc-sections -nostdlib
+x86-ASFLAGS   := -f elf64 -F dwarf -g
+x86-RUSTFLAGS := --emit obj -C target-cpu=$(x86-cpu) -C target-feature=$(x86-attr) -C code-model=kernel -C debuginfo=2 -C opt-level=3
+
+x86-ge   := multiboot init ge
+x86-ge-o := $(addprefix target/x86/, $(addsuffix .o, $(x86-ge)))
+x86-ge-d := $(addprefix target/x86/, $(addsuffix .d, $(x86-ge)))
+
+x86: target/x86/ge
+
+qemu-x86: target/x86/grub.iso
+	qemu-system-x86_64 -m 64M -name karyon -cdrom $< -boot d -display sdl -monitor vc
+
+target/x86/grub.iso: target/x86/ge
+	grub-mkrescue -o $@ $^ arch/x86/iso
+
+target/x86/ge: arch/x86/ge.ld $(x86-ge-d) $(x86-ge-o)
+	$(x86-LD) $(x86-LDFLAGS) -T arch/x86/ge.ld -o $@ $(x86-ge-o)
+
+target/x86/%.d: arch/x86/%.s
+	$(x86-AS) $(x86-ASFLAGS) -M -MF $@ $<
+
+target/x86/%.d: arch/x86/%.rs
+	touch $@
+
+target/x86/%.o: arch/x86/%.s
+	$(x86-AS) $(x86-ASFLAGS) -o $@ $<
+
+target/x86/%.o: arch/x86/%.rs
+	$(x86-RUSTC) $(x86-RUSTFLAGS) -o $@ $<
+
+.PHONY: x86 x86-qemu
+
+-include $(x86-ge-d)
